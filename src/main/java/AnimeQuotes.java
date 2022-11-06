e38095d160ffb58de500449c56c84b9abb565486
Dan Wright <wrightdj99@gmail.com>
2022-11-05 22:44:31 -0500

    Added Comments For Clarity

--- src/main/java/AnimeQuotes.java
+++ src/main/java/AnimeQuotes.java
@@ -1,28 +1,21 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
+//Ah what the heck...
+import java.net.*;
+import java.io.*;
 
 public class AnimeQuotes {
-
     public static void main(String[] args) throws IOException {
+        //Queue of requests from clients and the port number
         int qlen = 6;
         int port = 5050;
+        //Creating a new ServerSocket to bind our normal socket to.
         ServerSocket serverSocket = new ServerSocket(port, qlen);
         Socket sock;
-        /*try {
-            sock = serverSocket.accept();
-            new WebServerWorker(sock).start();
-            System.out.println("Connected.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
         System.out.println("Dan Wright's Anime Quote Server is running at port 5050. Give it a try!");
         while(true) {
+            //Accepting the connection and creating a new thread of the WebServer; we're making a new worker thread.
             sock = serverSocket.accept();
             new WebServerWorker(sock).start();
         }
--- src/main/java/WebServerWorker.java
+++ src/main/java/WebServerWorker.java
@@ -1,7 +1,6 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -10,13 +9,19 @@ import java.io.*;
 import java.net.*;
 
 public class WebServerWorker extends Thread{
+    //Declaring a socket/class level variables
     Socket sock;
     WebServerWorker(Socket s){sock = s;}
+    //String builder to create the new HTML form
     StringBuilder htmlString = new StringBuilder();
+    //These two things won't do much. The buffered reader will read in the URL being sent
+    //by the HTML UI, but the request will always be the same.
+    //The print stream will output the anime quote information to the end-user. More on that later.
     PrintStream out = null;
     BufferedReader in = null;
-    JSONParser parser = new JSONParser();
+    //JSON object to hold the response body from the anime quotes API
     JSONObject jo = new JSONObject();
+    //We will respond with this each time the user hits submit.
     String htmlResponse = "<!DOCTYPE html>" +
             "<html>" +
             "<head>" +
@@ -32,16 +37,19 @@ public class WebServerWorker extends Thread{
 
     public void run(){
         String inputRequest;
+        //Starting the HTML response...
         htmlString.append(htmlResponse);
         try{
             out = new PrintStream(sock.getOutputStream());
             in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            out.println("HTTP/1.1 200 OK");
-            out.println("Connection: close"); // Can fool with this.
-            out.println("Content-Length: 1000"); // Lazy, so set high. Calculate later.
-            out.println("Content-Type: text/html \r\n\r\n");
+            //Method to send out HTTP Headers
+            sendHeaders();
+            //Reading in the request from the HTML page
             inputRequest = in.readLine();
+            //Will always be true, but setting this up as an if statement if I ever want to add more functionality than
+            //just returning a random quote
             if(inputRequest != null && inputRequest.contains("getQuote")) {
+                //Code from RapidAPI that this program is subscribed to. Fairly cut and dry
                 HttpRequest request = HttpRequest.newBuilder()
                         .uri(URI.create("https://anime-quotes1.p.rapidapi.com/api/random"))
                         .header("X-RapidAPI-Key", "cc1d38da1fmsh73b309d9673daccp1c4109jsnf02b4cfb0a4e")
@@ -49,30 +57,43 @@ public class WebServerWorker extends Thread{
                         .method("GET", HttpRequest.BodyPublishers.noBody())
                         .build();
                 HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                //Putting the single HTTP Response from the API into this JSON object. We'll be parsing it in a sec.
                 jo.put("HTTPResponse", response.body());
-                System.out.println(jo.get("HTTPResponse"));
-                System.out.println("TRYING PARSING: \n\n");
+                //For testing functionality:
+                /*System.out.println(jo.get("HTTPResponse"));
+                System.out.println("TRYING PARSING: \n\n");*/
+                //PARSING THE STRING by colons and commas to extract each bit of information. May seem a bit
+                //cumbersome, but for this application our payload from the API will generally be pretty small. It'll be fine.
                 String parsedString = jo.get("HTTPResponse").toString();
                 String[] res = parsedString.split("[,::]", 6);
-                for(String r : res){
-                    System.out.println(r + "\n");
-                }
+                //Because I'm using a piecemeal method to pick this JSON apart, I'll need to take off the pesky closing curly brace.
                 res[5] = res[5].substring(0, res[5].length() - 1);
+                //Appending onto the HTML string
                 htmlString.append("<p>ANIME: " + res[1] + "</p>");
                 htmlString.append("<p>CHARACTER: " + res[3] + "</p>");
                 htmlString.append("<p>QUOTE: " + res[5] + "</p>");
+                //Serializing the API response by saving it to JSON.
                 FileWriter fileWriter = new FileWriter("src/main/savedTitles.json");
-                fileWriter.write(jo.toJSONString());
+                fileWriter.append(jo.toJSONString());
                 fileWriter.close();
             }
+            //Closing off the HTML string/document and sending it off
             htmlString.append("</body>");
-            htmlString.append("<html");
+            htmlString.append("</html>");
             out.println(htmlString);
+            //Close the socket and be done!
             sock.close();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void sendHeaders(){
+        out.println("HTTP/1.1 200 OK");
+        out.println("Connection: close"); //Standard HTTP header
+        out.println("Content-Length: 1000"); // Setting high for long quotes
+        out.println("Content-Type: text/html \r\n\r\n");
     }
 }
